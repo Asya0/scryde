f0daa250d1e1a2ad3a3c25ddd741b4b08516ef3f
Anuar <amendubaev@yandex.ru>
2023-12-02 20:04:06 +0600

    feat: christmas page

--- assets/js/script.js
+++ assets/js/script.js
@@ -188,3 +188,26 @@ for (let i = 0; i < tableButtons.length; i++) {
 }
 
 const fun = () => { };
+
+
+
+// christmas
+var countDownDate = new Date("Dec 7, 2023 13:00:00").getTime();
+
+var x = setInterval(function () {
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML = days + "дней " + hours + "часов " + minutes + "минут " + seconds + "секунд ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
